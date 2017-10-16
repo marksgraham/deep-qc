a240eb1cbfe03fea94604874610a064d0d72ab31
Mark Graham <mark.graham.13@ucl.ac.uk>
2017-10-16 14:29:33 +0100

    Edit previous entries

--- code/manual_qc.py
+++ code/manual_qc.py
@@ -1,12 +1,28 @@
 import argparse
 import numpy as np
+
+
 def manual_qc(num_volumes):
     '''Allows you to manually input indices for corrupted volumes.'''
     y_human = np.zeros(num_volumes)
     for i in range(num_volumes):
-        a = input('Vol '+str(i)+'. Enter 0 for OK, 1 for borderline, 2 for bad: \n')
-        if a == '1' or a =='2':
+        a = input('Vol '+str(i)+'. Enter 0 for OK, 1 for borderline, 2 for bad, 3 for other or ''e'' to edit previous: \n')
+        #Edit mode
+        if a =='e':
+            index_to_edit=0
+            while(index_to_edit!='q'):
+                index_to_edit = input('Edit mode. Enter index to alter or ''q'' to quit: \n')
+                if index_to_edit == 'q':
+                    a = input('Vol '+str(i)+'. Enter 0 for OK, 1 for borderline, 2 for bad, 3 for other: \n')
+                    break
+                else:
+                    print('Vol',index_to_edit,'has value',y_human[int(index_to_edit)])
+                    new_value = input('Enter new value: \n')
+                    y_human[int(index_to_edit)] = int(new_value)
+
+        if a == '1' or a =='2' or a =='3':
             y_human[int(i)] = int(a)
+
         else:
             y_human[int(i)] = 0
     return y_human
